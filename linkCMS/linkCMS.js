8787cd5d7754dec80420f84b00694ece6937deb3
Jose Manuel Romero <jmromero@MacBook-Pro-de-Administrador.local>
2023-04-13 16:51:26 +0200

    HZN extension v.1

--- linkCMS/linkCMS.js
+++ linkCMS/linkCMS.js
@@ -1,22 +1,31 @@
 function linkToCMS() {
-    const { pathname } = window.location;
-    const articleID = pathname.replace(/[^0-9]+/g, "");
-  
+  const data = __NEXT_DATA__;
+  if (data) {
+    const json = JSON.parse(data.childNodes[0]['textContent']);
+    const { articleID } = json.props.pageProps.articleStats;
     if (articleID) {
-      const url = 'https://pub.sandbox-hello.gcpp.io/articles/' + articleID;
-      console.log(url);
-      location.replace(url);
+      const url = `https://pub.sandbox-hello.gcpp.io/articles/${articleID}`;
+      const { hostname} = window.location;
+
+      const glideMode = hostname === 'qa.hellomagazine.com' ? 'qa' :
+                        hostname === 'www.hellomagazine.com' ? 'prod' :
+                        'dev';
+
+      const text = `Before you go to edit this article in the CMS make sure you are on ${glideMode} in Glide. Press OK if you are already in the right glide mode.`;
+      if (confirm(text) === true) {
+        window.open(url, "_blank");
+      }
     } else {
       console.log(':HZN Extension: This is not an article');
     }
   }
+}
   
-  chrome.action.onClicked.addListener((tab) => {
-    if (!tab.url.includes('chrome://')) {
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: linkToCMS
-      });
-    }
-  });
-  
+chrome.action.onClicked.addListener((tab) => {
+  if (!tab.url.includes('chrome://')) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: linkToCMS
+    });
+  }
+});
